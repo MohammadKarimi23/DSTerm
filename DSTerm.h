00fbe6af867f0c084fd54f682be7862b5cd95ccb
Mohammad <eecs.mk74@gmail.com>
2014-12-29 22:30:11 +0330

    Project for DS!!!!

---
+++ DSTerm.h
@@ -0,0 +1,32 @@
+
+// DSTerm.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CDSTermApp:
+// See DSTerm.cpp for the implementation of this class
+//
+
+class CDSTermApp : public CWinApp
+{
+public:
+	CDSTermApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CDSTermApp theApp;
